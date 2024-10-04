98e3f48c3d6d36ad41b59ee20210f776e98840b5
SongHyunsoo <sj960126@naver.com>
2024-10-04 17:13:26 +0900

    init : core ui init

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.shs.buildsrc.AndroidConfig
+import com.shs.buildsrc.implementationAndroidXCore
+import com.shs.buildsrc.implementationCompose
+import com.shs.buildsrc.implementationKotlin
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.shs.core.ui"
+    compileSdk = AndroidConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = AndroidConfig.MIN_SDK
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementationKotlin()
+    implementationAndroidXCore()
+    implementationCompose()
+}
