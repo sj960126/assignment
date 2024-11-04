2cc535821bb316e34f89cb83a3de2d6cf49c259d
SongHyunsoo <sj960126@naver.com>
2024-11-04 13:41:57 +0900

    fix : lint 수정

--- feature/main/src/main/java/com/shs/main/MainActivity.kt
+++ feature/main/src/main/java/com/shs/main/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import com.shs.ui.design_system.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
